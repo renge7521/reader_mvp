28a32df09529d616ccdedc5017bfe87bce542510
renge7521 <tetsuji_@ezweb.ne.jp>
2025-08-13 11:40:59 +0900

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/reader_mvp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/reader_mvp/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.app
+package com.example.reader_mvp  // ← 実際の namespace に合わせる
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
